1c81aa8d1eafcc61b04d39748f00f224a962e3a2
Aalaa-Fahim <143665741+Aalaa-Fahim@users.noreply.github.com>
2024-10-12 23:01:44 +0300

    comment cards

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import Home from './Pages/Home';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer';
-import card_cover_1 from './assets/images/card_cover_1.jpg';
+/* import card_cover_1 from './assets/images/card_cover_1.jpg';
 import card_cover_2 from './assets/images/card_cover_2.jpg';
 import card_cover_3 from './assets/images/card_cover_3.jpg';
 import card_cover_4 from './assets/images/card_cover_4.jpg';
-
+*/
 const App = () => {
   return (
     <div>
@@ -19,10 +19,10 @@ const App = () => {
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/beauty' element={<StoreCategory card={card_cover_1} category="beauty" />} />
+        /* <Route path='/beauty' element={<StoreCategory card={card_cover_1} category="beauty" />} />
         <Route path='/facial' element={<StoreCategory card={card_cover_2} category="facial" />} />
         <Route path='/hair' element={<StoreCategory card={card_cover_3} category="hair" />} />
-        <Route path='/baby' element={<StoreCategory card={card_cover_4} category="baby" />} />
+        <Route path='/baby' element={<StoreCategory card={card_cover_4} category="baby" />} /> */
         <Route path='/product' element={<Product />}>
           <Route path=':productId' element={<Product/>} />
         </Route>

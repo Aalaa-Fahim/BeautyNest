404d895a9279523071ed06720a6da3f9c45d482a
Aalaa-Fahim <143665741+Aalaa-Fahim@users.noreply.github.com>
2024-10-13 18:06:11 +0300

    Updated Footer Height

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,13 +4,13 @@ import styled from 'styled-components';
 const FooterContainer = styled.footer`
   background-color: #333;
   color: #fff;
-  padding: 20px;
+  padding: 10px; /* Decreased height */
   text-align: center;
 `;
 
 const FooterContent = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   flex-wrap: wrap;
 `;
@@ -24,8 +24,7 @@ const Footer = () => {
     <FooterContainer>
       <FooterContent>
         <FooterSection>
-          <h2>BeautyNest</h2>
-          <p>Your beauty and self-care destination.</p>
+          <p>&copy; 2024 BeautyNest. All rights reserved.</p> {/* Text moved to the left */}
         </FooterSection>
         <FooterSection>
           <h3>Contact Us</h3>
@@ -33,11 +32,12 @@ const Footer = () => {
         </FooterSection>
         <FooterSection>
           <h3>Follow Us</h3>
-          <p><a href="#" style={{ color: '#fff' }}>Instagram</a></p>
-          <p><a href="#" style={{ color: '#fff' }}>Facebook</a></p>
+          <p>
+            <a href="#" style={{ color: '#fff', marginRight: '10px' }}>Instagram</a>
+            <a href="#" style={{ color: '#fff' }}>Facebook</a> {/* Placed side by side */}
+          </p>
         </FooterSection>
       </FooterContent>
-      <p>&copy; {new Date().getFullYear()} BeautyNest. All rights reserved.</p>
     </FooterContainer>
   );
 };

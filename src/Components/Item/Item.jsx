3ff52afb5a98f35884ef0dc18037d45d50b90857
Aalaa-Fahim <143665741+Aalaa-Fahim@users.noreply.github.com>
2024-10-14 13:48:47 +0300

    changing the color of products description

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Item = (props) => {
   return (
-    <div className='bg-pink-100 shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105 flex flex-col justify-between min-h-[350px] w-full max-w-[200px] sm:max-w-[220px] md:max-w-[250px] mb-8'>
+    <div className='bg-[#daa49a] shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105 flex flex-col justify-between min-h-[350px] w-full max-w-[200px] sm:max-w-[220px] md:max-w-[250px] mb-8'>
       {/* Product Image Section */}
       <div className='flex justify-center items-center overflow-hidden rounded-md mb-4'>
         <img 
@@ -14,11 +14,11 @@ const Item = (props) => {
 
       {/* Product Name and Prices Section */}
       <div className='flex-grow'>
-        <p className='text-left text-sm font-bold text-purple-950'>
+        <p className='text-left text-sm font-bold text-[#695c5c]'>
           {props.name}
         </p>
         <div className='flex justify-between items-center mt-2'>
-          <div className='text-pink-600 font-semibold text-md'>
+          <div className='text-[#695c5c] font-semibold text-md'>
             ${props.new_price}
           </div>
           <div className='text-gray-400 line-through text-xs'>
@@ -30,7 +30,7 @@ const Item = (props) => {
       {/* Add to Cart Button */}
       <div className='mt-4'>
         <button 
-          className='bg-purple-600 text-white font-semibold py-2 px-4 rounded-lg w-full transition-colors hover:bg-purple-700 mt-auto'
+          className='bg-[#695c5c] text-white font-semibold py-2 px-4 rounded-lg w-full transition-colors hover:bg-purple-700 mt-auto'
         >
           Add To Cart
         </button>

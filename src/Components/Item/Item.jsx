3d655158d7f29ff56e54533e94fde985e7c6cbab
Reham <r_nour30@yahoo.com>
2024-10-14 08:00:06 +0300

    modify the ui of item and bestsale component

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,20 +2,38 @@ import React from 'react';
 
 const Item = (props) => {
   return (
-    <div className='bg-pink-100 shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105'>
-      <img 
-        src={props.image} 
-        alt='Item Image' 
-        className='w-full h-48 object-cover mx-auto rounded-md'
-      />
-      <p className='text-left text-base font-bold text-purple-950 mt-2'>{props.name}</p>
-      <div className='flex justify-between items-center mt-2'>
-        <div className='text-pink-600 font-semibold text-lg'>
-          ${props.new_price}
+    <div className='bg-pink-100 shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105 flex flex-col justify-between min-h-[350px] w-full max-w-[200px] sm:max-w-[220px] md:max-w-[250px] mb-8'>
+      {/* Product Image Section */}
+      <div className='flex justify-center items-center overflow-hidden rounded-md mb-4'>
+        <img 
+          src={props.image} 
+          alt='Item Image' 
+          className='w-36 h-36 object-cover'
+        />
+      </div>
+
+      {/* Product Name and Prices Section */}
+      <div className='flex-grow'>
+        <p className='text-left text-sm font-bold text-purple-950'>
+          {props.name}
+        </p>
+        <div className='flex justify-between items-center mt-2'>
+          <div className='text-pink-600 font-semibold text-md'>
+            ${props.new_price}
+          </div>
+          <div className='text-gray-400 line-through text-xs'>
+            ${props.old_price}
+          </div>
         </div>
-        <div className='text-gray-400 line-through text-sm'>
-          ${props.old_price}
-        </div>
+      </div>
+
+      {/* Add to Cart Button */}
+      <div className='mt-4'>
+        <button 
+          className='bg-purple-600 text-white font-semibold py-2 px-4 rounded-lg w-full transition-colors hover:bg-purple-700 mt-auto'
+        >
+          Add To Cart
+        </button>
       </div>
     </div>
   );

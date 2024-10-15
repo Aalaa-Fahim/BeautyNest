da95c32ab3b312584add3e6b946da63edf48d9b9
Aalaa-Fahim <alaahussin.322001@gmail.com>
2024-10-16 00:04:23 +0300

    adjusting cards size and motion

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -4,12 +4,12 @@ import 'aos/dist/aos.css';
 import AOS from 'aos';
 
 const Categories = () => {
-  // Define an array of categories, each containing a name, link, and image path
+  // Define an array of categories, each containing a name and a link
   const categories = [
-    { name: 'Beauty', link: '/beauty', image: 'src/assets/images/beauty1.jpg' },
-    { name: 'Facial', link: '/facial', image: 'src/assets/images/facial2.jpg' },
-    { name: 'Hair', link: '/hair', image: 'src/assets/images/hair2.jpg' },
-    { name: 'Baby', link: '/baby', image: 'src/assets/images/baby.jpg' },
+    { name: 'Beauty', link: '/beauty', image: '/src/assets/images/beauty1.jpg' },
+    { name: 'Facial', link: '/facial', image: '/src/assets/images/facial2.jpg' },
+    { name: 'Hair', link: '/hair', image: '/src/assets/images/hair2.jpg' },
+    { name: 'Baby', link: '/baby', image: '/src/assets/images/baby.jpg' },
   ];
 
   // useEffect to initialize AOS animations on component mount
@@ -19,19 +19,19 @@ const Categories = () => {
   }, []);
 
   return (
-    <div
-      className='container mx-auto px-8 py-8'
-      data-aos="flip-right"
-    >
-      <h1 className='text-4xl font-bold text-left mb-6' style={{ color: '#daa49a' }}>
+    <div className='container mx-auto px-8 py-8'>
+      {/* Heading of the Categories section */}
+      <h1 className='text-4xl font-bold text-left mb-6 sticky top-0 bg-white z-10' style={{ color: '#daa49a' }}>
         Categories
       </h1>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
+      
+      {/* Grid layout for categories */}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4' data-aos="flip-right">
         {categories.map((category, index) => (
-          <div key={index} className='bg-[#f3d6cf] shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105 flex flex-col justify-center items-center'>
+          <div key={index} className='bg-[#f3d6cf] shadow-lg rounded-lg p-4 transition-transform transform hover:scale-105 flex flex-col justify-center items-center max-w-[200px]'>
             {/* Display the category image */}
             <img
-              src={require(`${category.image}`)}
+              src={category.image} 
               alt={category.name}
               className='rounded-full w-24 h-24 mb-4'
             />

bed747efd641bd4318eb07918999c8d78d4b8c00
Reham <r_nour30@yahoo.com>
2024-10-15 08:59:15 +0300

    testing user to sign in successfully

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,13 +5,21 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  
 
   const LogIn = async () => {
     try {
+      if (!email || !password) {
+        setErrorMessage("Email and Password are required");
+        return;
+      }
       await signInWithEmailAndPassword(auth, email, password);
       console.log("User signed in successfully");
     } catch (error) {
       console.error("Error signing in:", error);
+      setErrorMessage(error.message);
     }
     
   };
@@ -19,13 +27,13 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
-        <h1 className="text-2xl font-bold mb-6 text-center">SingUp</h1>
+        <h1 className="text-2xl text-[#695c5c] font-bold mb-6 text-center">Login</h1>
         <input className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#695c5c]"
         placeholder='Enter Your Email' onChange={(e) => setEmail(e.target.value)} />
         <input className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-[#695c5c]"
         type="password" 
         placeholder='Passowrd' onChange={(e) => setPassword(e.target.value)}/>
-        <button className="w-full bg-[#695c5c] text-white py-2 rounded-lg hover:bg-goldenPink transition duration-200" onClick={LogIn}>Sign In</button>
+        <button className="w-full bg-[#695c5c] text-white py-2 rounded-lg hover:bg-goldenPink transition duration-200" onClick={LogIn}>Login</button>
       </div>
     </div>
   )

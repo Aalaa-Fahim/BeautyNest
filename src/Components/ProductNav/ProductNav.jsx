5716311f46ac9e61d026fe362c1a2c0c3c7955a0
Reham <r_nour30@yahoo.com>
2024-10-17 02:22:07 +0300

    update productNav style

--- src/Components/ProductNav/ProductNav.jsx
+++ src/Components/ProductNav/ProductNav.jsx
@@ -4,8 +4,10 @@ import { Link } from 'react-router-dom';
 const ProductNav = (props) => {
   const {product} = props;
   return (
-    <div className='max-w-screen-xl mx-auto flex pt-20'>
-      <Link to="/">Home</Link>{">"}<Link to={`/${product.category}`}> {product.category}</Link>{">"}<Link to={`/product/${product.id}`}>{product.name}</Link>
+    <div className='max-w-screen-xl mx-auto flex pt-20 items-center gap-2 py-4 px-4 lg:px-0 text-sm text-gray-600'>
+      <Link to="/" className='hover:text-goldenPink transition-colors'>Home</Link>
+      <span className='text-gray-500'>{">"}</span>
+      <Link to={`/${product.category}`}> {product.category}</Link>{">"}<Link to={`/product/${product.id}`}>{product.name}</Link>
     </div>
   )
 }

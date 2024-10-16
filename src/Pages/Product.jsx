afc8f351a6b90b166d0a4e72e7c897ec823ebe58
Reham <r_nour30@yahoo.com>
2024-10-16 12:47:29 +0300

    product.jsx page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StoreContext } from '../Context/StoreContext';
+import { use } from 'framer-motion/client';
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
+  const {all_product} = useContext(StoreContext);
+  const {productId} = useParams();
+  const catch_product = all_product.find((e) => e.id === (productId));
   return (
     <div>Product</div>
   )

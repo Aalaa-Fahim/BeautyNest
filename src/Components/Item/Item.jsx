7f0e7acdaefc37c342bcb048249d7d387541bfed
Reham <r_nour30@yahoo.com>
2024-10-16 13:07:43 +0300

    fix link import product image with product id

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
+import { Link } from 'react-router-dom';
 
 const Item = (props) => {
   useEffect(() => {

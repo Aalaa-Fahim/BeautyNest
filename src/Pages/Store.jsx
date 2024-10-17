28f572e94da5e732277077cde3341e475243d8f8
Reham <r_nour30@yahoo.com>
2024-10-17 05:11:58 +0300

    add route for store page

--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import all_data_product from '../assets/all_product';
+import Item from '../Item/Item';
 
 const Store = () => {
   return (
     <div className='container mx-auto px-8 py-8'>
       <h1 className='text-4xl font-bold text-left mb-6' style={{ color: '#daa49a' }}>
-        Best Sale Products
+        All Store Products
       </h1>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2'>
-        {data_product.map((item, index) => (
+        {all_data_product.map((item, index) => (
           <Item 
             key={index} 
             id={item.id} 

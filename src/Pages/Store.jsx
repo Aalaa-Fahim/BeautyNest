a6ebfb881848766bb9d659b1c2eebd66e4b2444c
Reham <r_nour30@yahoo.com>
2024-10-17 05:34:35 +0300

    update store page

--- src/Pages/Store.jsx
+++ src/Pages/Store.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import all_data_product from '../assets/all_product';
 import Item from '../Components/Item/Item';
 
-const Store = () => {
+const Store = (props) => {
   return (
-    <div className='container mx-auto px-8 py-8'>
-      <h1 className='text-4xl font-bold text-left mb-6' style={{ color: '#daa49a' }}>
-        All Store Products
-      </h1>
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2'>
-        {all_data_product.map((item, index) => (
+    
+    <div className='container mx-auto px-4 py-8'>
+      <div className='max-w-4xl mx-auto mb-6'>
+        <img 
+          src={props.banner} 
+          alt='category banner' 
+          className='w-full h-auto object-cover rounded-lg'
+          style={{ maxHeight: '400px' }}
+        />
+      </div>
+
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mx-auto max-w-screen-xl'>
+      {all_data_product.map((item, index) => (
           <Item 
             key={index} 
             id={item.id} 
